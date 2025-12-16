6f6830dc0c3b30b02a1d712b08e90a5c8904cc78
Claudy2312 <softwareengineer66623@gmail.com>
2025-12-16 20:40:49 +0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello this is my deployment on eks 🚀");
+  res.send("Hello this is my deployment on eks by nouman 🚀");
 });
 
 app.listen(3000, () => console.log("App running on port 3000"));
