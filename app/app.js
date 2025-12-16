d90eca7aa09025b48db0600b5017a956e6f1f900
Muhammad Nouman <nouman.shafi@systemsltd.com>
2025-12-16 20:07:40 +0500

    nnnn

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from EC2 Docker 🚀");
+  res.send("Hello this is my deployment on eks 🚀");
 });
 
 app.listen(3000, () => console.log("App running on port 3000"));
